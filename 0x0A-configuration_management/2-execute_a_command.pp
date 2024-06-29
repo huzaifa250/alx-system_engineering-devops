ff95033cddbaa231fb305d185c67f523863fa920
huzaifa250 <huz.dark1@gmail.com>
2024-06-28 17:55:42 -0700

    done

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,16 @@
+# Ensure procps package is installed to provide pkill command
+
+package { 'procps':
+  ensure => installed,
+}
+
+
+
 # Kills a process named killmenow
 
 exec { 'pkill killmenow' :
-  path     => 'usr/bin/',
+  path     => '/usr/bin:/bin:/usr/sbin:/sbin',
   command  => 'pkill killmenow',
   provider => 'shell'
+  require  => Package['procps'],
     }
